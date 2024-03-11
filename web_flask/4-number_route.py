eb847e2102046953e76d81cb5b80fbb0dc245b41
Greentaylor27 <greentaylor18@gmail.com>
2024-03-11 22:12:35 +0000

    Importing render_template

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module that should display two inputs"""
-from flask import Flask
+from flask import Flask , render_template
 
 
 app = Flask(__name__)
@@ -30,6 +30,11 @@ def python(text='is cool'):
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_num(n):
     return f'{n} is a number'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_template(n:int):
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
