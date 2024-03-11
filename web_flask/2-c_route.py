a0767841a31385c6daf73b246dd51b28a800a99f
Greentaylor27 <greentaylor18@gmail.com>
2024-03-11 21:31:58 +0000

    Adding method not_not_index

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that should display two inputs"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def not_index():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def not_not_index(txt):
+    return 'C ' + txt.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
