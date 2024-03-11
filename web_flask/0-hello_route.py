7be9650eb11de4b6653fcbce77e871423acc395a
Greentaylor27 <greentaylor18@gmail.com>
2024-03-11 20:03:42 +0000

    Seeing what error I get

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,3 +2,12 @@
 """Backend api that will hopefully display Hello HBNB"""
 from flask import Flask
 
+
+app = Flask(__name__)
+
+@app.route('/api/hello')
+def hello_world():
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
