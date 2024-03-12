96e1c741047f1c65a428faf14836846036362d98
Greentaylor27 <greentaylor18@gmail.com>
2024-03-12 19:44:52 +0000

    Forgot _template in the url

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -32,7 +32,7 @@ def is_num(n):
     return f'{n} is a number'
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n:int):
     return render_template('5-number.html', n=n)
 
